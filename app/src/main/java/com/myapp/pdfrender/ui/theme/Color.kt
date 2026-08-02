4b271c86058a5ffac4c509984ce07177e23111c9
bariskarapinar <bkarapinar1@hotmail.com>
2026-08-02 13:36:27 +0300

    Color Update

---
+++ app/src/main/java/com/myapp/pdfrender/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.myapp.pdfrender.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Ultra Mega Neon Palette - Now with more "VIBE"
+val NeonPink = Color(0xFFFF10F0)
+val NeonCyan = Color(0xFF00FFFF)
+val NeonPurple = Color(0xFF9400D3)
+val NeonYellow = Color(0xFFFFF000)
+val NeonGreen = Color(0xFF39FF14)
+
+val DeepSpace = Color(0xFF0B0E14)
+val DeepIndigo = Color(0xFF1A1A2E)
+val GlassWhite = Color(0x33FFFFFF)
+val GlassBlack = Color(0x66000000)
